e1a235aea6a25b12debd6ad54dce9188ad381e2b
laquilas <lucasaquila@hotmail.com>
2022-12-08 10:58:20 -0400

    init

---
+++ example02/py_test.py
@@ -0,0 +1,14 @@
+import pytest
+
+def test_foo_bar1():
+   assert True
+
+def test_foo_bar2():
+   assert True
+
+def test_foo_bar3():
+   assert True
+
+
+if __name__ == "__main__":
+    test_foo_bar1()
